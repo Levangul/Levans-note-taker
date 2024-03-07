c612e3e6a85a384dfcf399af2c9827d94f232a8d
Levangul <150108077+Levangul@users.noreply.github.com>
2024-03-07 02:19:25 -0500

    finalcode

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 
 const path = require("path");
 const express = require("express");
-const router = express.Router();
+const router = express.Router();    
 
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public", "notes.html"));
--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require("express");
-const path = require("path"); // Import the 'path' module
+const path = require("path"); 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Import routes
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
-// Middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Use routes
 app.use(htmlRoutes);
 app.use(apiRoutes);
 
-// Catch-all route for non-existent routes
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
